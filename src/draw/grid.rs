931003d4c883cead8a12766b79251465a7db5542
llGaetanll <gaetan.almela@gmail.com>
2024-03-20 13:38:34 -0400

    committing changes to grid

--- src/draw/grid.rs
+++ src/draw/grid.rs
@@ -6,7 +6,7 @@ use crate::draw::window::WindowData;
 use crate::ext::sdl::ColorInterpolationExt;
 
 pub struct GridData {
-    /// multiplier on `DEFAULT_CELL_SIZE`
+    /// multiplier on `base_cell_size`
     zoom_level: u32,
 
     /// When zooming in or out, this is the multiplier
@@ -18,13 +18,17 @@ pub struct GridData {
     // precomputed for interpolation when computing gridline colors
     max_zoom_level: u32,
 
-    x: i32,
-    y: i32,
+    // `window_offsets[i]` represents the window offset for `zoom_level` `i`. The reason that
+    // offsets are stored this way is to avoid loss of precision that comes with floating point
+    // numbers. This set of coordinates allows us completely reconstruct the position of the camera
+    // on the grid.
+    window_offsets: Vec<(i32, i32)>,
+
     show_gridlines: bool,
 
     // base color of the gridlines
     gridline_color: Color,
-    background_color: Color
+    background_color: Color,
 }
 
 impl GridData {
@@ -39,8 +43,8 @@ impl GridData {
             base_cell_size,
             max_zoom_level: Self::get_max_zoom_level(base_cell_size, zoom_mult),
 
-            x: 0,
-            y: 0,
+            window_offsets: vec![(0, 0)],
+
             show_gridlines: true,
 
             gridline_color: Color::RGB(60, 60, 60),
@@ -52,42 +56,44 @@ impl GridData {
         self.show_gridlines = !self.show_gridlines;
     }
 
-    pub fn shift_x(&mut self, shift: i32) {
-        let spacing = self.line_spacing();
-
-        if let Some(spacing) = spacing {
-            let spacing = spacing as i32;
-            self.x = (((self.x + shift) % spacing) + spacing) % spacing;
-        }
+    pub fn shift_x(&mut self, dx: i32) {
+        Self::offset(self, dx, 0);
     }
 
-    pub fn shift_y(&mut self, shift: i32) {
-        let spacing = self.line_spacing();
+    pub fn shift_y(&mut self, dy: i32) {
+        Self::offset(self, 0, dy);
+    }
 
-        if let Some(spacing) = spacing {
-            let spacing = spacing as i32;
-            self.y = (((self.y + shift) % spacing) + spacing) % spacing;
-        }
+    pub fn offset(&mut self, dx: i32, dy: i32) {
+        self.window_offsets[self.zoom_level as usize].0 += dx;
+        self.window_offsets[self.zoom_level as usize].1 += dy;
+    }
+
+    pub fn reset_zoom(&mut self) {
+        self.zoom_level = 0;
     }
 
     pub fn zoom_in(&mut self) {
         if self.zoom_level == 0 {
-            return
+            return;
         }
 
         self.zoom_level -= 1;
 
         // update x and y offsets
-        self.shift_x(0);
-        self.shift_y(0);
+        self.offset(0, 0);
     }
 
     pub fn zoom_out(&mut self) {
         self.zoom_level += 1;
 
+        // only add to window_offsets if needed
+        if self.zoom_level as usize == self.window_offsets.len() {
+            self.window_offsets.push((0, 0));
+        }
+
         // update x and y offsets
-        self.shift_x(0);
-        self.shift_y(0);
+        self.offset(0, 0);
     }
 
     pub fn line_spacing(&self) -> Option<u32> {
@@ -110,16 +116,22 @@ impl GridData {
         // draw grid lines
         if let Some(spacing) = spacing {
             if !self.show_gridlines {
-                return Ok(())
+                return Ok(());
             }
 
             let gridline_color = self.get_gridline_color();
             canvas.set_draw_color(gridline_color);
 
+            let (dx, dy) = self.window_offsets[self.zoom_level as usize];
+            let (dx, dy) = (
+                (dx + dx.abs() * spacing as i32) % spacing as i32,
+                (dy + dy.abs() * spacing as i32) % spacing as i32,
+            );
+
             // vertical lines
             let num_lines = window.width.div_ceil(spacing);
             for i in 0..num_lines {
-                let delta = self.x + (i * spacing) as i32;
+                let delta = dx + (i * spacing) as i32;
 
                 canvas.draw_line(
                     Point::new(delta, 0),
@@ -130,7 +142,7 @@ impl GridData {
             // horizontal lines
             let num_lines = window.height.div_ceil(spacing);
             for i in 0..num_lines {
-                let delta = self.y + (i * spacing) as i32;
+                let delta = dy + (i * spacing) as i32;
 
                 canvas.draw_line(Point::new(0, delta), Point::new(window.width as i32, delta))?;
             }
